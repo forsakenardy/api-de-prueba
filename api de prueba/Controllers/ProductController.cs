ad4d13251cc65608fdf0a7b77e993e45bab7a2e2
forsakenardy <nardy.allstars@gmail.com>
2025-09-04 10:44:13 +0200

    Add project files.

---
+++ api de prueba/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using api_de_prueba.Models;
+using Microsoft.AspNetCore.Mvc;
+using api_de_prueba.Services;
+
+namespace api_de_prueba.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _service;
+
+        public ProductController(ProductService service)
+        {
+            _service = service;
+        }
+
+        // GET api/products
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAll());
+
+        // GET api/products/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = _service.GetById(id);
+            return product is not null ? Ok(product) : NotFound();
+        }
+
+        // POST api/products
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            var newProduct = _service.Add(product);
+            return CreatedAtAction(nameof(GetById), new { id = newProduct.Id }, newProduct);
+        }
+
+        // PUT api/products/1
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product updatedProduct)
+        {
+            var result = _service.Update(id, updatedProduct);
+            return result ? NoContent() : NotFound();
+        }
+
+        // DELETE api/products/1
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result = _service.Delete(id);
+            return result ? NoContent() : NotFound();
+        }
+    }
+}
---
+++ api de prueba/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace api_de_prueba.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
